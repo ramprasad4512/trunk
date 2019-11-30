66357fbbecdcc7af76cd39f2d578b840110dda16
Ramprasad Reddy <50319320+ramprasad4512@users.noreply.github.com>
2019-11-30 13:17:24 +0530

    add polym

--- sample.py
+++ sample.py
@@ -20,3 +20,15 @@ emp2 = Employee("Manni", 5000)
 emp1.displayEmployee()
 emp2.displayEmployee()
 print "Total Employee %d" % Employee.empCount
+
+
+class Parent:        # define parent class
+   def myMethod(self):
+      print 'Calling parent method'
+
+class Child(Parent): # define child class
+   def myMethod(self):
+      print 'Calling child method'
+
+c = Child()          # instance of child
+c.myMethod()         # child calls overridden method
